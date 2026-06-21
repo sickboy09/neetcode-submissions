5db835958dcc8fdda290826f57ab8dde0b381037
Sagnick Bhowmick <70048625+sickboy09@users.noreply.github.com>
2026-06-22 04:05:56 +0530

    Add: design-hashmap - submission-4

---
+++ Data Structures & Algorithms/design-hashmap/submission-4.py
@@ -0,0 +1,20 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.hmap = {}
+
+    def put(self, key: int, value: int) -> None:
+        self.hmap[key] = value
+
+    def get(self, key: int) -> int:
+        return self.hmap.get(key, -1)
+
+    def remove(self, key: int) -> None:
+        self.hmap.pop(key, None)
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
