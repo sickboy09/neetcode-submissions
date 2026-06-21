50af893ba1a9f60e42671f617d45556d2043a58f
Sagnick Bhowmick <70048625+sickboy09@users.noreply.github.com>
2026-06-22 04:33:40 +0530

    Add: design-hashmap - submission-14

---
+++ Data Structures & Algorithms/design-hashmap/submission-14.py
@@ -0,0 +1,34 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.maxl = 1000001
+        self.hmap = [[] for _ in range(self.maxl)]
+
+    def put(self, key: int, value: int) -> None:
+        index = key % self.maxl
+        for pair in self.hmap[index]:
+            if pair[0] == key:
+                pair[1] = value
+                return
+        self.hmap[index].append([key, value])
+
+    def get(self, key: int) -> int:
+        index = key % self.maxl
+        for pair in self.hmap[index]:
+            if pair[0] == key:
+                return pair[1]
+        return -1
+
+    def remove(self, key: int) -> None:
+        index = key % self.maxl
+        for pair in self.hmap[index]:
+            if pair[0] == key:
+                pair[1] = -1
+                # self.hmap[index].remove(tup)
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
