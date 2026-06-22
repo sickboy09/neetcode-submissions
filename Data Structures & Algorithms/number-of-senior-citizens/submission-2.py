38074d75b178f4a7ff62d28ca0a15e8e7dedd286
Sagnick Bhowmick <70048625+sickboy09@users.noreply.github.com>
2026-06-23 03:24:52 +0530

    Add: number-of-senior-citizens - submission-2

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        # count = 0
+        # for passenger in details:
+        #     if int(passenger[11:13]) > 60:
+        #         count += 1
+        # return count
+        count = 0
+        passengers = {x: int(x[11:13]) for x in details}
+        for key, value in passengers.items():
+            if value > 60:
+                count += 1
+        return count
+
